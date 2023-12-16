7b6f10275f383080aba7c2e506e0316d4785e969
Mariam B <121524324+stanmicro9@users.noreply.github.com>
2023-12-16 04:02:19 +0200

    Update GymDataBase.java

--- src/DATABASE/GymDataBase.java
+++ src/DATABASE/GymDataBase.java
@@ -7,14 +7,14 @@ public class GymDataBase {
     public static <T> void saveData(ArrayList<T> dataList, String fileName) {
         try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(fileName))) {
             outputStream.writeObject(dataList);
-            System.out.println("\nData saved successfully.\n");
+            outputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
     public static <T> ArrayList<T> loadData(String fileName) {
         try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(fileName))) {
-            return (ArrayList<T>) inputStream.readObject();
+            return (ArrayList<T>)  inputStream.readObject();
         } catch (FileNotFoundException e) {
             System.out.println("\nFile not found. Creating a new list.\n");
             return new ArrayList<>();

5b8f869162170bbca10adec28822f6c832d60cbf
Mariam B <121524324+stanmicro9@users.noreply.github.com>
2023-12-17 03:16:12 +0200

    Update Subscription.java
    subs

--- src/SERVICES/Subscription.java
+++ src/SERVICES/Subscription.java
@@ -1,12 +1,7 @@
 package SERVICES;
 
-import USERS.*;
-import SERVICES.Membership_plan;
 import java.io.Serializable;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
-import static GYM.GYM.input;
+
 
 public class Subscription implements Serializable {
     protected int customerID;
@@ -31,27 +26,28 @@ public class Subscription implements Serializable {
         this.coachID=coachID;
         this.plan=plan;
     }
+    public Subscription(){}
     public void displaySubscription(){
         System.out.println("Customer id : "+getCustomerID()+"\nCoach id : "+getCoachID()+"\nMembership_plan: ");
         plan.displayPlan();
     }
 
-    public void renewSubscription() {
-        LocalDate currentDate = LocalDate.now();
-
-        if (currentDate.isAfter(plan.getEndDate())) {
-            System.out.println("Your subscription has expired. Do you want to renew? (yes/no): ");
-            String renewChoice = input.next();
-
-            if ("yes".equalsIgnoreCase(renewChoice)) {
-                // Handle the renewal logic here
-                System.out.println("Subscription renewed successfully!");
-            } else {
-                System.out.println("Renewal cancelled.");
-            }
-        }
-        else {
-            throw new IllegalStateException("You cannot renew before your current subscription expires.");
-        }
-    }
+//    public void renewSubscription() {
+//        LocalDate currentDate = LocalDate.now();
+//
+//        if (currentDate.isAfter(plan.getEndDate())) {
+//            System.out.println("Your subscription has expired. Do you want to renew? (yes/no): ");
+//            String renewChoice = input.next();
+//
+//            if ("yes".equalsIgnoreCase(renewChoice)) {
+//                // Handle the renewal logic here
+//                System.out.println("Subscription renewed successfully!");
+//            } else {
+//                System.out.println("Renewal cancelled.");
+//            }
+//        }
+//        else {
+//            throw new IllegalStateException("You cannot renew before your current subscription expires.");
+//        }
+//    }
 }

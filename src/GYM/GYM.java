53798655577a51c19c10743c1d0e1dea6fa6b432
Mariam B <121524324+stanmicro9@users.noreply.github.com>
2023-12-17 03:12:23 +0200

    Update GYM.java
    gym

--- src/GYM/GYM.java
+++ src/GYM/GYM.java
@@ -2,11 +2,12 @@ package GYM;
 
 import USERS.*;
 import SERVICES.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Scanner;
 import USERS.Customer;
 
-public class GYM {
+public class GYM implements Serializable {
     public String name="GYM EL MOR3BEEEN";
     public String address;
     public int phoneNumber;
@@ -26,7 +27,7 @@ public class GYM {
     public static Scanner input = new Scanner(System.in);
     public static void regCustomer(){
         System.out.println("\nName: ");
-        String regName=input.nextLine();
+        String regName=input.next();
         System.out.println("Address: ");
         String regAddress=input.nextLine();
         //--------------
@@ -43,12 +44,16 @@ public class GYM {
         System.out.println("Password: ");
         String regPassword=input.next();
 
-        if(USER.validateName(regName) && USER.validateEmail(regEmail) && USER.validatePhone(regNumber) && USER.validatePassword(regPassword))
+        if(USER.validateName(regName) && USER.validateEmail(regEmail) && USER.validatePhone(regNumber))
         {
+            System.out.println("\n\nEnter Subscription and Plan Details\n");
+            System.out.println("\nEnter plan start date (yyyy-MM-dd): ");
+            String inputStartDate = input.next();
+
             Customer newCustomer=new Customer(regAddress,regEmail,regName,regPassword,regGender,regNumber);
+            Subscription(newCustomer,inputStartDate);
             userList.add(newCustomer);
             System.out.println("Registration complete");
-
         }
         else if(!USER.validateName(regName)){
             System.out.println("\nInvalid name please try again.\nName must not contain special characters and nor exceed 29 characters in length.\n");
@@ -59,18 +64,17 @@ public class GYM {
         else if(!USER.validatePhone(regNumber)){
             System.out.println("\nInvalid, phone number registered before or not written correctly!\n11 digits form is needed.\nPlease try again.\n");
         }
-        else if(!USER.validatePassword(regPassword)){
-            System.out.println("\nPassword shouldn't contain spaces.\nPlease try again.\n");
-        }
 
-    } //shaklaha 7lww awyyyyy <3, proud of myself walahy
+    }
 
     public static void regCoach(){
         System.out.println("\nName: ");
-        String regName=input.nextLine();
-        input.nextLine();
+        String regName=input.next();
         System.out.println("Address: ");
         String regAddress=input.nextLine();
+        //--------------
+        input.nextLine();
+        //--------------
         System.out.println("Phone Number: ");
         int regNumber=input.nextInt();
         System.out.println("Gender (F/M): ");
@@ -84,13 +88,11 @@ public class GYM {
         System.out.println("Password: ");
         String regPassword=input.next();
 
-        if(USER.validateName(regName) && USER.validateEmail(regEmail) && USER.validatePhone(regNumber) && USER.validatePassword(regPassword))
+        if(USER.validateName(regName) && USER.validateEmail(regEmail) && USER.validatePhone(regNumber))
         {
             Coach newCoach=new Coach(regAddress,regEmail,regName,regPassword,regGender,regNumber,regWorkingHr);
             userList.add(newCoach);
             System.out.println("\nOperation Done Successfully!\n");
-
-            //System.out.println(userList);
         }
         else if(!USER.validateName(regName)){
             System.out.println("\nInvalid name please try again.\nName must not contain special characters and nor exceed 29 characters in length.\n");
@@ -100,9 +102,6 @@ public class GYM {
         }
         else if(!USER.validatePhone(regNumber)){
             System.out.println("\nInvalid, phone number registered before or not written correctly!\n11 digits form is needed.\nPlease try again.\n");
-        }
-        else if(!USER.validatePassword(regPassword)){
-            System.out.println("\nPassword shouldn't contain spaces.\nPlease try again.\n");
         }
     }
 
@@ -121,25 +120,40 @@ public class GYM {
 
         while (true){
             GYM.coachesList();
-            System.out.println("\nChoose a coach ID to get assigned to: ");
+            System.out.println("Choose a coach ID to get assigned to: ");
             assignedCoachID=input.nextInt();
-            Coach assignedCoach=Coach.getCoachByID(assignedCoachID);
-            Customer[] customersArray = assignedCoach.getCustomersArray();
-            for (int i=0;i<10;i++){
-                if (customersArray[9]!=null) {
-                    System.out.println("\nThis coach is full, choose another one and try again.\n");
-                    break;
-                }
-                else if (customersArray[i]==null){
-                    assignedCoach.setCustomersArray(i,Customer);
-                    return assignedCoachID;
+            boolean checker=false;
+
+            for (USER user : GYM.userList) {
+                if (user instanceof Coach) {
+                    Coach coach = (Coach) user; //downcasting
+                    if (assignedCoachID==coach.getCoachID()) {
+                        checker=true;
+                        break;
+                    }
                 }
             }
+            if (checker){
+                Coach assignedCoach=Coach.getCoachByID(assignedCoachID);
+                Customer[] customersArray = assignedCoach.getCustomersArray();
+                for (int i=0;i<10;i++){
+                    if (customersArray[9]!=null) {
+                        System.out.println("This coach is full, choose another one and try again.");
+                        break;
+                    }
+                    else if (customersArray[i]==null){
+                        assignedCoach.setCustomersArray(i,Customer);
+                        return assignedCoachID;
+                    }
+                }
+            }
+            else System.out.println("This coach ID doesn't exist.");
+
         }
     }
 
     public static void Subscription(Customer customer, String inputStartDate){
-        int chosenCoachID=GYM.addCustomerToSpecificCoach(customer);
+        int chosenCoachID=addCustomerToSpecificCoach(customer);
         int chosenBundle= Membership_plan.planOptions();
         System.out.println("\nEnter the number of months (1-12): m7dsh ydkhal 13, mt2rfonash b2a");
         int chosenMonths=input.nextInt();
@@ -167,30 +181,4 @@ public class GYM {
             }
         }
     }
-
-
-//de hya hyaha el subscription bs feha checker zyada for indexing, h7tagha? idk yet
-//    public static void chosenSubscription(Customer customer, String inputStartDate){
-//        Scanner subsInput=new Scanner(System.in);
-//
-//        int index=-1;
-//
-//        int chosenCoachID=GYM.addCustomerToSpecificCoach(customer);
-//        for(int i=0;i<12;i++){
-//            if(customer.subs[i+1]==null){
-//                index=i;
-//            }
-//        }
-//        customer.subs[index+1].setCoachID(chosenCoachID);
-//
-//        int chosenBundle= Membership_plan.planOptions();
-//        System.out.println("\nEnter the number of months (1-12): m7dsh ydkhal 13, mt2rfonash b2a");
-//        int chosenMonths=subsInput.nextInt();
-//        Membership_plan chosenPlan=new Membership_plan(inputStartDate, chosenBundle, chosenMonths);
-//        Subscription newSubs=new Subscription(customer.getCustomerID(), chosenCoachID, chosenPlan);
-//        customer.setSubs(newSubs);
-//
-//        subsInput.close();
-//
-//    }
 }
